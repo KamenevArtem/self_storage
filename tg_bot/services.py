805541a52d322b960dc94ca1c1708b3219fbc563
Stranix <Phantom2525@gmail.com>
2023-04-22 14:01:02 +0300

    Add получение просроченных заказов из бд

--- tg_bot/services.py
+++ tg_bot/services.py
@@ -1,5 +1,8 @@
+import datetime
+
 from telegram import KeyboardButton
 from telegram import ReplyKeyboardMarkup
+from tg_bot.models import Order
 
 
 def create_tg_keyboard_markup(
@@ -7,7 +10,6 @@ def create_tg_keyboard_markup(
         buttons_per_row: int = 3,
         need_start: bool = False
 ) -> ReplyKeyboardMarkup:
-
     keyboard_buttons = [KeyboardButton(text) for text in buttons_text]
 
     rows = [
@@ -22,3 +24,30 @@ def create_tg_keyboard_markup(
         resize_keyboard=True,
         one_time_keyboard=True
     )
+
+
+def get_finish_rent_time(order: Order) -> datetime:
+    finish_rent_time = order.order_start_date + datetime.timedelta(
+        days=order.rent_time
+    )
+    return finish_rent_time
+
+
+def get_failed_orders_from_db() -> list[Order] | None:
+    non_closed_orders = Order.objects.filter(
+        completed=False,
+        rent_time__isnull=False
+    )
+
+    if not non_closed_orders:
+        return
+
+    current_date = datetime.datetime.now().date()
+    failed_orders = []
+    for order in non_closed_orders:
+        if current_date > get_finish_rent_time(order):
+            if not order.completed:
+                failed_orders.append(order)
+
+    if failed_orders:
+        return failed_orders
